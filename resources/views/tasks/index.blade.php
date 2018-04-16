e11013a0beed6343b8a68a625684cbe6c03616e3
ayumi.itou <drittes_auge17nehmen@outlook.jp>
2018-04-16 17:50:35 +0000

    ok

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>メッセージ一覧</h1>
+    <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
         <ul>
@@ -12,6 +12,6 @@
         </ul>
     @endif
 
-    {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
+    {!! link_to_route('tasks.create', '新規タスクの作成') !!}
 
 @endsection
