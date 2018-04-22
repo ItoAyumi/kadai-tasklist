353ae704b156f2367d34ec2ae981986a59e5d91e
ayumi.itou <drittes_auge17nehmen@outlook.jp>
2018-04-22 12:39:55 +0000

    bootstrap-kadai

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,20 +2,28 @@
 
 @section('content')
 
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+            
     <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-    
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::submit('作成') !!}
+                {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
 
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
